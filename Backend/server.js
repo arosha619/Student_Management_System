5bca22131d733712eb88105d97f09523d34c3fcd
arosha619 <90087992+arosha619@users.noreply.github.com>
2022-06-07 22:09:37 +0530

    completed backend

---
+++ Backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const bodyparser=require("body-parser");
+const app =express();
+
+require("dotenv").config();
+
+const PORT= process.env.PORT || 8080; //  when hosting to the server can assign available port or local machine port =8070 
+
+app.use(cors());
+app.use(bodyparser.json());
+
+//database connection
+
+const URL =process.env.MONGODB_URL;
+mongoose.connect(URL);
+const connection =mongoose.connection;
+connection.once("open",()=>{
+    console.log("mongodb Connection success!");
+})
+
+const studentRouter= require("./routes/students");
+app.use("/student",studentRouter);
+
+
+app.listen(PORT,()=>{
+    console.log('server is up and runing on port:',PORT);
+})
