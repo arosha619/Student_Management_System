a54a189768ff58114ffe43c4fd4b143e32e3b529
arosha619 <90087992+arosha619@users.noreply.github.com>
2022-06-08 23:43:39 +0530

    complete add student to databse

--- frontend/src/components/AddStudent.js
+++ frontend/src/components/AddStudent.js
@@ -1,6 +1,29 @@
 import React, { useState } from "react";
+import axios from "axios";
+
 
 function AddStudent() {
+
+const [name,setName]=useState("");
+const [age,setAge]=useState("");
+const [gender,setGender]=useState("");
+
+ function sendData(e){
+   e.preventDefault();
+
+   const newStudent={
+     name,age,gender
+   }
+   
+   axios.post("http://localhost:8080/student/add",newStudent).then(()=>{
+     alert("student added!")
+   }).catch((err)=>{
+    alert(err)
+   })
+   
+
+ }
+
   return (
     <div>
       <form>
@@ -11,6 +34,8 @@ function AddStudent() {
             class="form-control"
             id="Student_Name"
             placeholder="Enter Student Name"
+            onChange={(e)=>{setName(e.target.value);}}
+            required
           />
         </div>
         <div class="form-group">
@@ -20,6 +45,8 @@ function AddStudent() {
             class="form-control"
             id="Student_age"
             placeholder="Enter Student age"
+            onChange={(e)=>{setAge(e.target.value);}}
+            required
           />
         </div>
         <div class="form-group">
@@ -29,10 +56,12 @@ function AddStudent() {
             class="form-control"
             id="Student_gender"
             placeholder="Enter Student Gender"
+            onChange={(e)=>{setGender(e.target.value);}}
+            required
           />
         </div>
 
-        <button type="submit" class="btn btn-primary">
+        <button type="submit" onClick={sendData} class="btn btn-primary">
           Submit
         </button>
       </form>
